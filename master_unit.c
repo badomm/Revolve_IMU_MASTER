6bfe49eceef07c40450f57e4aeea9d29a1fe1682
badomm <jimmy.chau.no@gmail.com>
2015-03-07 18:48:38 +0100

    added project file

---
+++ master_unit.c
@@ -0,0 +1,24 @@
+/*
+ * master_unit.c
+ *
+ * Created: 3/7/2015 18:46:39
+ *  Author: Jimmy Chau
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
